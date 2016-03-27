e3995b45d587b0017063e7d9d27b4db91dc03a62
lowellmower <lowell.mower@gmail.com>
2016-03-27 12:27:59 -0400

    test to try connection between version/method

--- lib/restfully.rb
+++ lib/restfully.rb
@@ -1,6 +1,13 @@
-require "restfully/version"
+require 'net/http'
+require 'uri'
+
+require 'restfully/version'
+require 'restfully/request'
 
 module Restfully
 
-
+	def self.version
+		Restfully::VERSION
+	end
+	
 end
